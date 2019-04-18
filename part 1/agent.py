a2df2a224d12c400990c4ab268c3bd06049c5a62
Jack Gentile <jack.t.gentile@gmail.com>
2019-04-18 10:34:31 -0500

    to be finished on EWS

--- part 1/agent.py
+++ part 1/agent.py
@@ -4,7 +4,7 @@ import random
 
 
 class Agent:
-    
+
     def __init__(self, actions, Ne, C, gamma):
         self.actions = actions
         self.Ne = Ne # used in exploration function
@@ -17,7 +17,7 @@ class Agent:
 
     def train(self):
         self._train = True
-        
+
     def eval(self):
         self._train = False
 
@@ -34,6 +34,67 @@ class Agent:
         self.s = None
         self.a = None
 
+    def discretize_state(state):
+        snake_head_x = state[0]
+        snake_head_y = state[1]
+        snake_body = state[2]
+        food_x = state[3]
+        food_y = state[4]
+        # state vars
+        adj_wall_x = 0
+        adj_wall_y = 0
+        food_dir_x = 0
+        food_dir_y = 0
+        adj_body_top = 0
+        adj_body_bottom = 0
+        adj_body_left = 0
+        adj_body_right = 0
+
+        # assign food_dir_y
+        if snake_head_x == food_x:
+            food_dir_x = 0
+        elif snake_head_x > food_x:
+            food_dir_x = 1
+        else:
+            food_dir_x = 2
+        # assign food_dir_y
+        if snake_head_y == food_y:
+            food_dir_y = 0
+        elif snake_head_y > food_y:
+            food_dir_y = 1
+        else:
+            food_dir_y = 2
+        # assign body state vars
+        for body_segment in snake_body:
+            # check top
+            if adj_body_top == 0 and (body_segment[0] == snake_head_x and body_segment[1] == snake_head_y-1):
+                adj_body_top = 1
+                continue
+            # check bottom
+            if adj_body_bottom == 0 and (body_segment[0] == snake_head_x and body_segment[1] == snake_head_y+1):
+                adj_body_bottom = 1
+                continue
+            # check left
+            if adj_body_left == 0 and (body_segment[1] == snake_head_y and body_segment[0] == snake_head_x-1):
+                adj_body_left = 1
+                continue
+            # check right
+            if adj_body_right == 0 and (body_segment[1] == snake_head_y and body_segment[0] == snake_head_x+1):
+                adj_body_right = 1
+                continue
+        # assign adj_wall_x
+        if snake_head_x == utils.WALL_SIZE:
+            adj_wall_x = 1
+        elif snake_head_x == (utils.DISPLAY_SIZE - utils.WALL_SIZE - utils.GRID_SIZE):
+            adj_wall_x = 2
+        # assign adj_wall_y
+        if snake_head_y == utils.WALL_SIZE:
+            adj_wall_y = 1
+        elif snake_head_y == (utils.DISPLAY_SIZE - utils.WALL_SIZE - utils.GRID_SIZE):
+            adj_wall_y = 2
+        # return values
+        return adj_wall_x, adj_wall_y, food_dir_x, food_dir_y, adj_body_top, adj_body_bottom, adj_body_left, adj_body_right
+
     def act(self, state, points, dead):
         '''
         :param state: a list of [snake_head_x, snake_head_y, snake_body, food_x, food_y] from environment.
@@ -47,5 +108,46 @@ class Agent:
         (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the 480x480 board)
 
         '''
+        # calculate environment variables from state
+        adj_wall_x, adj_wall_y, food_dir_x, food_dir_y, adj_body_top,
+            adj_body_bottom, adj_body_left, adj_body_right = discretize_state(state)
 
-        return self.actions[0]
+        # copy arrays for easy reads
+        N_values = self.N[adj_wall_x,adj_wall_y,food_dir_x,food_dir_y,adj_body_top,adj_body_bottom,adj_body_left,adj_body_right,:]
+        Q_values = self.Q[adj_wall_x,adj_wall_y,food_dir_x,food_dir_y,adj_body_top,adj_body_bottom,adj_body_left,adj_body_right,:]
+        # determine a with max f value according to documentation
+        best_action = self.actions[0]
+        best_f = -1
+        for a in self.actions:
+            cur_f = Q_values[a]
+            if N_values[a] < self.Ne:
+                cur_f = 1
+            if cur_f > best_f:
+                best_action = a
+                best_f = cur_f
+        # update N table
+        self.N[adj_wall_x,adj_wall_y,food_dir_x,food_dir_y,adj_body_top,adj_body_bottom,adj_body_left,adj_body_right,best_action] += 1
+        # if training, update Q table values
+        if not self.train:
+            return best_action
+        learning_rate = self.C / (self.C + N_values[best_action] + 1)
+        # determine s' (the next state after best_action is taken)
+        if best_action == 0:
+            # move up
+            state[1] -= utils.GRID_SIZE
+        elif best_action == 1:
+            # move down
+            state[1] += utils.GRID_SIZE
+        elif best_action == 2:
+            # move left
+            state[0] -= utils.GRID_SIZE
+        else:
+            # move right
+            state[0] += utils.GRID_SIZE
+        # recalculate environment vars
+        adj_wall_x, adj_wall_y, food_dir_x, food_dir_y, adj_body_top,
+            adj_body_bottom, adj_body_left, adj_body_right = discretize_state(state)
+        # TODO calculate delta
+        delta = 0
+
+        return best_action
